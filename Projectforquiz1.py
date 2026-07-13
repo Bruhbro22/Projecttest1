9a21bc2ec426a01f72245e6504cc7484cbf2fba2
Bruhbro22 <donmadhu411@gmail.com>
2026-07-13 17:20:42 -0500

    Add files via upload

---
+++ Projectforquiz1.py
@@ -0,0 +1,72 @@
+print("Welcome to Anish's number guessing game, guess a number that is in the range of 1-50, You have ❤️ ❤️ ❤️❤️ ❤️  lives remaining.")
+secret=27
+bro=int(input("Pick a number from 1-50 and it cannot be a decimal: "))
+i=0
+while i<5:
+    if bro in range(41,51):
+        print("🧊 Ice cold")
+    elif bro=='27':
+        print("You guessed the secret number!")
+    elif bro in range(30,40):
+        print("🥶 Cold")
+    elif bro in range(1,11):
+        print("🧊 Ice cold")
+    elif bro in range(12,21):
+        print("🥶 Cold")
+    elif bro in range(22,24):
+        print("🌡️ Warm")
+    elif bro in range(28,29):
+        print("🔥 Hot")
+    else:
+        i=i+1
+    while i<5:
+        if bro in range(41,51):
+            print("🧊 Ice cold")
+        elif bro=='27':
+            print("You guessed the secret number!")
+        elif bro in range(30,40):
+            print("🥶 Cold")
+        elif bro in range(1,11):
+            print("🧊 Ice cold")
+        elif bro in range(12,21):
+            print("🥶 Cold")
+        elif bro in range(22,24):
+            print("🌡️ Warm")
+        elif bro in range(28,29):
+            print("🔥 Hot")
+        else:
+            i=i+1
+        while i<5:
+            if bro in range(41,51):
+                print("🧊 Ice cold")
+            elif bro=='27':
+                print("You guessed the secret number!")
+            elif bro in range(30,40):
+                print("🥶 Cold")
+            elif bro in range(1,11):
+                print("🧊 Ice cold")
+            elif bro in range(12,21):
+                print("🥶 Cold")
+            elif bro in range(22,24):
+                print("🌡️ Warm")
+            elif bro in range(28,29):
+                print("🔥 Hot")
+    else:
+        i=i+1
+    while i<5:
+                if bro in range(41,51):
+                    print("🧊 Ice cold")
+                elif bro=='27':
+                    print("You guessed the secret number!")
+                elif bro in range(30,40):
+                 print("🥶 Cold")
+                elif bro in range(1,11):
+                    print("🧊 Ice cold")
+                elif bro in range(12,21):
+                    print("🥶 Cold")
+                elif bro in range(22,24):
+                    print("🌡️ Warm")
+                elif bro in range(28,29):
+                    print("🔥 Hot")
+    else:
+        i=i+1
